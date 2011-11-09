0f62a28d42efedffe1e9d9c037bd2107e20d80af
cncfanatics <diego.duclos@gmail.com>
2011-11-09 21:49:07 +0100

    Add basic dataHandler ABC and a JSON based implementation (unfinished)

---
+++ dataHandler/__init__.py
@@ -0,0 +1,1 @@
+from .jsonDataHandler import JsonDataHandler
---
+++ dataHandler/dataHandler.py
@@ -0,0 +1,22 @@
+'''
+Created on 9-nov.-2011
+
+@author: cncfanatics
+'''
+
+from abc import ABCMeta
+from abc import abstractmethod
+
+class DataHandler(object):
+    '''
+    DataHandler abstract baseclass, it handles fetching relevant data from wherever it is stored
+    '''
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def getExpression(self, id):
+        ...
+
+    @abstractmethod
+    def getType(self, id):
+        ...
