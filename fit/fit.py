8ec1c05bdd2972722886e672eaaf4279489c7b0c
cncfanatics <diego.duclos@gmail.com>
2011-11-11 19:25:21 +0100

    Add Fit class

---
+++ fit/fit.py
@@ -0,0 +1,16 @@
+'''
+Created on 11-nov.-2011
+
+@author: cncfanatics
+'''
+
+class Fit(object):
+    '''
+    Fit object; Represents a single fitting with its shiptype, modules, rigs, subsystems and so on
+    '''
+
+
+    def __init__(self, shipType):
+        '''
+        Constructor
+        '''
