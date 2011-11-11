2d1610a027051b54a988496e48bb7659afaecffa
cncfanatics <diego.duclos@gmail.com>
2011-11-11 20:23:48 +0100

    Add license info (LGPL)

--- fit/fit.py
+++ fit/fit.py
@@ -2,6 +2,21 @@
 Created on 11-nov.-2011
 
 @author: cncfanatics
+
+This file is part of eos.
+
+eos is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Eos is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with eos.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 class Fit(object):
