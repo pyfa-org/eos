f3d8a618b86990f49f190d7d1ba4df00afab7e5c
DarkPhoenix <phoenix@mail.ru>
2011-12-29 19:45:57 +0400

    Add positive tests for unused gang modifiers

--- tests/infoBuilder/parseGangGrpMod.py
+++ tests/infoBuilder/parseGangGrpMod.py
@@ -4,34 +4,37 @@ from eos import const
 from eos.data.expression import Expression
 from eos.data.effect.builder import InfoBuilder
 
-class TestItmMod(TestCase):
-    """Test parsing of trees describing direct item modification"""
+class TestGangGrpMod(TestCase):
+    """Test parsing of trees describing gang-mates' ship modules modification filtered by group"""
 
     def testBuildSuccess(self):
-        eTgt = Expression(1, 24, value="Ship")
-        eTgtAttr = Expression(2, 22, attributeId=9)
+        # Manually composed example, as CCP doesn't use this modification type in any effect
+        eTgtGrp = Expression(1, 26, groupId=80)
+        eTgtAttr = Expression(2, 22, attributeId=158)
         eOptr = Expression(3, 21, value="PostPercent")
-        eSrcAttr = Expression(4, 22, attributeId=327)
-        eTgtSpec = Expression(5, 12, arg1=eTgt, arg2=eTgtAttr)
+        eSrcAttr = Expression(4, 22, attributeId=349)
+        eTgtSpec = Expression(5, 34, arg1=eTgtGrp, arg2=eTgtAttr)
         eOptrTgt = Expression(6, 31, arg1=eOptr, arg2=eTgtSpec)
-        eAddMod = Expression(7, 6, arg1=eOptrTgt, arg2=eSrcAttr)
-        eRmMod = Expression(8, 58, arg1=eOptrTgt, arg2=eSrcAttr)
+        eAddMod = Expression(7, 2, arg1=eOptrTgt, arg2=eSrcAttr)
+        eRmMod = Expression(8, 54, arg1=eOptrTgt, arg2=eSrcAttr)
         infos, status = InfoBuilder().build(eAddMod, eRmMod)
         self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
         self.assertEqual(len(infos), 1, msg="one info must be generated")
         info = infos.pop()
         expType = const.infoDuration
         self.assertEqual(info.type, expType, msg="info type must be duration (ID {})".format(expType))
-        expGang = False
+        expGang = True
         self.assertIs(info.gang, expGang, msg="info gang flag must be {}".format(expGang))
         expLocation = const.locShip
         self.assertEqual(info.location, expLocation, msg="info target location must be ship (ID {})".format(expLocation))
-        self.assertIsNone(info.filterType, msg="info target filter type must be None")
-        self.assertIsNone(info.filterValue, msg="info target filter value must be None")
+        expFilterType = const.filterGroup
+        self.assertEqual(info.filterType, expFilterType, msg="info target filter type must be group (ID {})".format(expFilterType))
+        expFilterValue = 80
+        self.assertEqual(info.filterValue, expFilterValue, msg="info target filter value must be {}".format(expFilterValue))
         expOperation = const.optrPostPercent
         self.assertEqual(info.operation, expOperation, msg="info operation must be PostPercent (ID {})".format(expOperation))
-        expTgtAttr = 9
+        expTgtAttr = 158
         self.assertEqual(info.targetAttributeId, expTgtAttr, msg="info target attribute ID must be {}".format(expTgtAttr))
-        expSrcAttr = 327
+        expSrcAttr = 349
         self.assertEqual(info.sourceAttributeId, expSrcAttr, msg="info source attribute ID must be {}".format(expSrcAttr))
         self.assertIsNone(info.conditions, msg="conditions must be None")
---
+++ tests/infoBuilder/parseGangOwnSrqMod.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+
+from eos import const
+from eos.data.expression import Expression
+from eos.data.effect.builder import InfoBuilder
+
+class TestGangOwnSrqMod(TestCase):
+    """Test parsing of trees describing gang-mates' in-space items modification filtered by skill requirement"""
+
+    def testBuildSuccess(self):
+        # Manually composed example, as CCP doesn't use this modification type in any effect
+        eTgtSrq = Expression(1, 29, typeId=3326)
+        eTgtAttr = Expression(2, 22, attributeId=654)
+        eOptr = Expression(3, 21, value="PostMul")
+        eSrcAttr = Expression(4, 22, attributeId=848)
+        eTgtSpec = Expression(5, 64, arg1=eTgtSrq, arg2=eTgtAttr)
+        eOptrTgt = Expression(6, 31, arg1=eOptr, arg2=eTgtSpec)
+        eAddMod = Expression(7, 4, arg1=eOptrTgt, arg2=eSrcAttr)
+        eRmMod = Expression(8, 56, arg1=eOptrTgt, arg2=eSrcAttr)
+        infos, status = InfoBuilder().build(eAddMod, eRmMod)
+        self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
+        self.assertEqual(len(infos), 1, msg="one info must be generated")
+        info = infos.pop()
+        expType = const.infoDuration
+        self.assertEqual(info.type, expType, msg="info type must be duration (ID {})".format(expType))
+        expGang = True
+        self.assertIs(info.gang, expGang, msg="info gang flag must be {}".format(expGang))
+        expLocation = const.locSpace
+        self.assertEqual(info.location, expLocation, msg="info target location must be space (ID {})".format(expLocation))
+        expFilterType = const.filterSkill
+        self.assertEqual(info.filterType, expFilterType, msg="info target filter type must be skill (ID {})".format(expFilterType))
+        expFilterValue = 3326
+        self.assertEqual(info.filterValue, expFilterValue, msg="info target filter value must be {}".format(expFilterValue))
+        expOperation = const.optrPostMul
+        self.assertEqual(info.operation, expOperation, msg="info operation must be PostMul (ID {})".format(expOperation))
+        expTgtAttr = 654
+        self.assertEqual(info.targetAttributeId, expTgtAttr, msg="info target attribute ID must be {}".format(expTgtAttr))
+        expSrcAttr = 848
+        self.assertEqual(info.sourceAttributeId, expSrcAttr, msg="info source attribute ID must be {}".format(expSrcAttr))
+        self.assertIsNone(info.conditions, msg="conditions must be None")
