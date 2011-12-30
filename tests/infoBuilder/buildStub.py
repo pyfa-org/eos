45ffe98cffe0009e98095f9ce5d161ea404390b9
DarkPhoenix <phoenix@mail.ru>
2011-12-30 23:05:17 +0400

    Add test for stub which uses different expressions

--- tests/infoBuilder/buildStub.py
+++ tests/infoBuilder/buildStub.py
@@ -7,23 +7,30 @@ from eos.data.effect.builder import InfoBuilder
 class TestItmMod(TestCase):
     """Test parsing of stub expressions"""
 
-    def testInt0BuildSuccess(self):
+    def testInt0StubBuildSuccess(self):
         preStub = Expression(1, 27, value="0")
         postStub = Expression(2, 27, value="0")
         infos, status = InfoBuilder().build(preStub, postStub)
         self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
         self.assertEqual(len(infos), 0, msg="no infos must be generated")
 
-    def testInt1BuildSuccess(self):
+    def testInt1StubBuildSuccess(self):
         preStub = Expression(1, 27, value="1")
         postStub = Expression(2, 27, value="1")
         infos, status = InfoBuilder().build(preStub, postStub)
         self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
         self.assertEqual(len(infos), 0, msg="no infos must be generated")
 
-    def testBoolTrueBuildSuccess(self):
+    def testBoolStubTrueBuildSuccess(self):
         preStub = Expression(1, 23, value="True")
         postStub = Expression(2, 23, value="True")
+        infos, status = InfoBuilder().build(preStub, postStub)
+        self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
+        self.assertEqual(len(infos), 0, msg="no infos must be generated")
+
+    def testDiffStubBuildSuccess(self):
+        preStub = Expression(1, 23, value="True")
+        postStub = Expression(2, 27, value="0")
         infos, status = InfoBuilder().build(preStub, postStub)
         self.assertEqual(status, const.effectInfoOkFull, msg="expressions must be successfully parsed")
         self.assertEqual(len(infos), 0, msg="no infos must be generated")
