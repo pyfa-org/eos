7ca15ed322ed3e60ff89ee8a49618c7b611a7378
DarkPhoenix <phoenix@mail.ru>
2013-09-11 11:42:00 +0400

    Adjust holder unordered container tests

--- tests/fit/holderContainer/unordered/testUnordered.py
+++ tests/fit/holderContainer/unordered/testUnordered.py
@@ -33,24 +33,23 @@ class TestContainerUnordered(ContainerTestCase):
         self.fitMock = self._setupContainerCheck()
         self.container = HolderSet(self.fitMock)
 
-    def testAddRemoveHolder(self):
+    def testAddHolder(self):
         container = self.container
         fitMock = self.fitMock
         holder1 = Mock(spec_set=())
         holder2 = Mock(spec_set=())
-        self.assertEqual(len(fitMock.mock_calls), 0)
+        fitCallsBefore = len(fitMock.mock_calls)
         container.add(holder1)
-        self.assertEqual(len(fitMock.mock_calls), 1)
-        self.assertEqual(fitMock.method_calls[0], call._addHolder(holder1))
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 1)
+        self.assertEqual(fitMock.method_calls[-1], call._addHolder(holder1))
+        fitCallsBefore = len(fitMock.mock_calls)
         container.add(holder2)
-        self.assertEqual(len(fitMock.mock_calls), 2)
-        self.assertEqual(fitMock.method_calls[1], call._addHolder(holder2))
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 1)
+        self.assertEqual(fitMock.method_calls[-1], call._addHolder(holder2))
         container.remove(holder1)
-        self.assertEqual(len(fitMock.mock_calls), 3)
-        self.assertEqual(fitMock.method_calls[2], call._removeHolder(holder1))
         container.remove(holder2)
-        self.assertEqual(len(fitMock.mock_calls), 4)
-        self.assertEqual(fitMock.method_calls[3], call._removeHolder(holder2))
         self.assertObjectBuffersEmpty(container)
 
     def testAddHolderFailure(self):
@@ -60,11 +59,33 @@ class TestContainerUnordered(ContainerTestCase):
         holder2 = Mock(spec_set=())
         container.add(holder1)
         fitMock._addHolder.side_effect = HolderAddError(holder2)
+        fitCallsBefore = len(fitMock.mock_calls)
         self.assertRaises(ValueError, container.add, holder2)
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 1)
+        self.assertEqual(fitMock.method_calls[-1], call._addHolder(holder2))
         self.assertTrue(holder1 in container)
         container.remove(holder1)
-        self.assertEqual(len(fitMock.mock_calls), 3)
         self.assertEqual(len(container), 0)
+        self.assertObjectBuffersEmpty(container)
+
+    def testRemoveHolder(self):
+        container = self.container
+        fitMock = self.fitMock
+        holder1 = Mock(spec_set=())
+        holder2 = Mock(spec_set=())
+        container.add(holder1)
+        container.add(holder2)
+        fitCallsBefore = len(fitMock.mock_calls)
+        container.remove(holder1)
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 1)
+        self.assertEqual(fitMock.method_calls[-1], call._removeHolder(holder1))
+        fitCallsBefore = len(fitMock.mock_calls)
+        container.remove(holder2)
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 1)
+        self.assertEqual(fitMock.method_calls[-1], call._removeHolder(holder2))
         self.assertObjectBuffersEmpty(container)
 
     def testRemoveHolderFailure(self):
@@ -75,10 +96,12 @@ class TestContainerUnordered(ContainerTestCase):
         container.add(holder1)
         container.add(holder2)
         container.remove(holder2)
+        fitCallsBefore = len(fitMock.mock_calls)
         self.assertRaises(KeyError, container.remove, holder2)
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 0)
         self.assertTrue(holder1 in container)
         container.remove(holder1)
-        self.assertEqual(len(fitMock.mock_calls), 4)
         self.assertEqual(len(container), 0)
         self.assertObjectBuffersEmpty(container)
 
@@ -139,10 +162,11 @@ class TestContainerUnordered(ContainerTestCase):
         holder2 = Mock(spec_set=())
         container.add(holder1)
         container.add(holder2)
-        self.assertEqual(len(fitMock.mock_calls), 2)
+        fitCallsBefore = len(fitMock.mock_calls)
         container.clear()
-        self.assertEqual(len(fitMock.mock_calls), 4)
-        newCalls = fitMock.method_calls[2:4]
+        fitCallsAfter = len(fitMock.mock_calls)
+        self.assertEqual(fitCallsAfter - fitCallsBefore, 2)
+        newCalls = fitMock.method_calls[-2:]
         self.assertIn(call._removeHolder(holder1), newCalls)
         self.assertIn(call._removeHolder(holder2), newCalls)
         self.assertEqual(len(container), 0)
