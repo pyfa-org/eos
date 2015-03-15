4dfa0eca1e39be9f992111d5215dd989baaa2fd8
DarkPhoenix <phoenix@mail.ru>
2015-03-15 18:33:50 +0300

    Add test to check source switching using literal string rather than Source object

--- tests/fit/specials/test_source_switch.py
+++ tests/fit/specials/test_source_switch.py
@@ -19,7 +19,7 @@
 #===============================================================================
 
 
-from unittest.mock import Mock, call
+from unittest.mock import Mock, call, patch
 
 from eos.const.eos import State
 from eos.fit.holder.container import HolderSet
@@ -27,15 +27,16 @@ from eos.source import Source
 from eos.tests.fit.environment import BaseHolder, CachingHolder
 from eos.tests.fit.fit_testcase import FitTestCase
 
-
-class TestFitEosSwitch(FitTestCase):
+@patch('eos.fit.fit.SourceManager')
+class TestFitSourceSwitch(FitTestCase):
 
     def make_fit(self, *args, **kwargs):
         fit = super().make_fit(*args, **kwargs)
         fit.container = HolderSet(fit, BaseHolder)
         return fit
 
-    def test_none_to_none(self):
+    def test_none_to_none(self, source_mgr):
+        source_mgr.default = None
         holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
         fit = self.make_fit(source=None)
         fit.container.add(holder)
@@ -43,6 +44,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_before = len(fit._link_tracker.mock_calls)
         rt_calls_before = len(fit._restriction_tracker.mock_calls)
         st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
         # Action
         fit.source = None
         # Checks
@@ -50,12 +52,15 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_after = len(fit._link_tracker.mock_calls)
         rt_calls_after = len(fit._restriction_tracker.mock_calls)
         st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
         self.assertEqual(holder_calls_after - holder_calls_before, 0)
         self.assertEqual(lt_calls_after - lt_calls_before, 0)
         self.assertEqual(rt_calls_after - rt_calls_before, 0)
         self.assertEqual(st_calls_after - st_calls_before, 0)
+        self.assertEqual(sm_calls_after - sm_calls_before, 0)
 
-    def test_none_to_source(self):
+    def test_none_to_source(self, source_mgr):
+        source_mgr.default = None
         source = Mock(spec_set=Source)
         holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
         fit = self.make_fit(source=None)
@@ -64,6 +69,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_before = len(fit._link_tracker.mock_calls)
         rt_calls_before = len(fit._restriction_tracker.mock_calls)
         st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
         fit._link_tracker.add_holder.side_effect = lambda h: self.assertIs(h._fit.source, source)
         fit._link_tracker.enable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
         fit._restriction_tracker.enable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
@@ -76,6 +82,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_after = len(fit._link_tracker.mock_calls)
         rt_calls_after = len(fit._restriction_tracker.mock_calls)
         st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
         self.assertEqual(holder_calls_after - holder_calls_before, 2)
         holder_calls = holder.mock_calls[-2:]
         self.assertIn(call._refresh_source(), holder_calls)
@@ -88,8 +95,10 @@ class TestFitEosSwitch(FitTestCase):
         self.assertEqual(st_calls_after - st_calls_before, 2)
         self.assertEqual(fit.stats.mock_calls[-2], call._clear_volatile_attrs())
         self.assertEqual(fit.stats.mock_calls[-1], call._enable_states(holder, {State.offline}))
+        self.assertEqual(sm_calls_after - sm_calls_before, 0)
 
-    def test_source_to_none(self):
+    def test_source_to_none(self, source_mgr):
+        source_mgr.default = None
         source = Mock(spec_set=Source)
         holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
         fit = self.make_fit(source=source)
@@ -98,6 +107,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_before = len(fit._link_tracker.mock_calls)
         rt_calls_before = len(fit._restriction_tracker.mock_calls)
         st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
         fit._link_tracker.disable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
         fit._link_tracker.remove_holder.side_effect = lambda h: self.assertIs(h._fit.source, source)
         fit._restriction_tracker.disable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
@@ -109,6 +119,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_after = len(fit._link_tracker.mock_calls)
         rt_calls_after = len(fit._restriction_tracker.mock_calls)
         st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
         self.assertEqual(holder_calls_after - holder_calls_before, 2)
         holder_calls = holder.mock_calls[-2:]
         self.assertIn(call._refresh_source(), holder_calls)
@@ -121,8 +132,10 @@ class TestFitEosSwitch(FitTestCase):
         self.assertEqual(st_calls_after - st_calls_before, 2)
         self.assertEqual(fit.stats.mock_calls[-2], call._disable_states(holder, {State.offline}))
         self.assertEqual(fit.stats.mock_calls[-1], call._clear_volatile_attrs())
+        self.assertEqual(sm_calls_after - sm_calls_before, 0)
 
-    def test_source_to_source(self):
+    def test_source_to_source(self, source_mgr):
+        source_mgr.default = None
         source1 = Mock(spec_set=Source)
         source2 = Mock(spec_set=Source)
         holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
@@ -132,6 +145,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_before = len(fit._link_tracker.mock_calls)
         rt_calls_before = len(fit._restriction_tracker.mock_calls)
         st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
         fit._link_tracker.disable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source1)
         fit._link_tracker.remove_holder.side_effect = lambda h: self.assertIs(h._fit.source, source1)
         fit._link_tracker.add_holder.side_effect = lambda h: self.assertIs(h._fit.source, source2)
@@ -148,6 +162,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_after = len(fit._link_tracker.mock_calls)
         rt_calls_after = len(fit._restriction_tracker.mock_calls)
         st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
         self.assertEqual(holder_calls_after - holder_calls_before, 2)
         holder_calls = holder.mock_calls[-2:]
         self.assertIn(call._refresh_source(), holder_calls)
@@ -164,8 +179,10 @@ class TestFitEosSwitch(FitTestCase):
         self.assertEqual(fit.stats.mock_calls[-3], call._disable_states(holder, {State.offline}))
         self.assertEqual(fit.stats.mock_calls[-2], call._clear_volatile_attrs())
         self.assertEqual(fit.stats.mock_calls[-1], call._enable_states(holder, {State.offline}))
+        self.assertEqual(sm_calls_after - sm_calls_before, 0)
 
-    def test_source_to_source_same(self):
+    def test_source_to_source_same(self, source_mgr):
+        source_mgr.default = None
         source = Mock(spec_set=Source)
         holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
         fit = self.make_fit(source=source)
@@ -174,6 +191,7 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_before = len(fit._link_tracker.mock_calls)
         rt_calls_before = len(fit._restriction_tracker.mock_calls)
         st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
         # Action
         fit.source = source
         # Checks
@@ -181,7 +199,49 @@ class TestFitEosSwitch(FitTestCase):
         lt_calls_after = len(fit._link_tracker.mock_calls)
         rt_calls_after = len(fit._restriction_tracker.mock_calls)
         st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
         self.assertEqual(holder_calls_after - holder_calls_before, 0)
         self.assertEqual(lt_calls_after - lt_calls_before, 0)
         self.assertEqual(rt_calls_after - rt_calls_before, 0)
         self.assertEqual(st_calls_after - st_calls_before, 0)
+        self.assertEqual(sm_calls_after - sm_calls_before, 0)
+
+    def test_none_to_literal_source(self, source_mgr):
+        source = Mock(spec_set=Source)
+        source_mgr.get.side_effect = lambda alias: source if alias == 'src_alias' else None
+        source_mgr.default = None
+        holder = Mock(_fit=None, state=State.offline, spec_set=CachingHolder(1))
+        fit = self.make_fit(source=None)
+        fit.container.add(holder)
+        holder_calls_before = len(holder.mock_calls)
+        lt_calls_before = len(fit._link_tracker.mock_calls)
+        rt_calls_before = len(fit._restriction_tracker.mock_calls)
+        st_calls_before = len(fit.stats.mock_calls)
+        sm_calls_before = len(source_mgr.mock_calls)
+        fit._link_tracker.add_holder.side_effect = lambda h: self.assertIs(h._fit.source, source)
+        fit._link_tracker.enable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
+        fit._restriction_tracker.enable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
+        fit.stats.enable_states.side_effect = lambda h, s: self.assertIs(h._fit.source, source)
+        holder._refresh_source.side_effect = lambda: self.assertIs(holder._fit.source, source)
+        # Action
+        fit.source = 'src_alias'
+        # Checks
+        holder_calls_after = len(holder.mock_calls)
+        lt_calls_after = len(fit._link_tracker.mock_calls)
+        rt_calls_after = len(fit._restriction_tracker.mock_calls)
+        st_calls_after = len(fit.stats.mock_calls)
+        sm_calls_after = len(source_mgr.mock_calls)
+        self.assertEqual(holder_calls_after - holder_calls_before, 2)
+        holder_calls = holder.mock_calls[-2:]
+        self.assertIn(call._refresh_source(), holder_calls)
+        self.assertIn(call._clear_volatile_attrs(), holder_calls)
+        self.assertEqual(lt_calls_after - lt_calls_before, 2)
+        self.assertEqual(fit._link_tracker.mock_calls[-2], call.add_holder(holder))
+        self.assertEqual(fit._link_tracker.mock_calls[-1], call.enable_states(holder, {State.offline}))
+        self.assertEqual(rt_calls_after - rt_calls_before, 1)
+        self.assertEqual(fit._restriction_tracker.mock_calls[-1], call.enable_states(holder, {State.offline}))
+        self.assertEqual(st_calls_after - st_calls_before, 2)
+        self.assertEqual(fit.stats.mock_calls[-2], call._clear_volatile_attrs())
+        self.assertEqual(fit.stats.mock_calls[-1], call._enable_states(holder, {State.offline}))
+        self.assertEqual(sm_calls_after - sm_calls_before, 1)
+        self.assertEqual(source_mgr.mock_calls[-1], call.get('src_alias'))
