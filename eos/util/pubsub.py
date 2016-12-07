6b22bef9184e64d6c15f53f996aae9973c7c1285
DarkPhoenix <phoenix@mail.ru>
2016-12-07 17:48:14 +0300

    Add utility classes to aid with publisher-subscriber pattern implementation

---
+++ eos/util/pubsub.py
@@ -0,0 +1,75 @@
+# ===============================================================================
+# Copyright (C) 2011 Diego Duclos
+# Copyright (C) 2011-2015 Anton Vorobyov
+#
+# This file is part of Eos.
+#
+# Eos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Eos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Eos. If not, see <http://www.gnu.org/licenses/>.
+# ===============================================================================
+
+
+"""
+Implementation of publish-subscribe pattern.
+"""
+
+
+from abc import ABCMeta, abstractmethod
+
+
+class MessageBroker:
+    """
+    Manages subscriptions and dispatches received
+    publications according to active subscriptions.
+    """
+
+    def __init__(self):
+        # Format: {event class: subscribers}
+        self.__subscribers = {}
+
+    def _subscribe(self, subscriber, event_class):
+        """
+        Register subscriber for an event class.
+        """
+        subscribers = self.__subscribers.setdefault(event_class, set())
+        subscribers.add(subscriber)
+
+    def _unsubscribe(self, subscriber, event_class):
+        """
+        Unregister subscriber from an event class.
+        """
+        try:
+            subscribers = self.__subscribers[event_class]
+        except KeyError:
+            return
+        subscribers.discard(subscriber)
+
+    def _publish(self, message):
+        """
+        Publish message and make sure that all
+        interested subscribers are notified.
+        """
+        subscribers = self.__subscribers.get(type(message), ())
+        for subscriber in subscribers:
+            subscriber._notify(message)
+
+
+class BaseSubscriber(metaclass=ABCMeta):
+    """
+    Base class for subscribers. Forces them to
+    implement methods all subscribers should have.
+    """
+
+    @abstractmethod
+    def _notify(self, message):
+        ...
