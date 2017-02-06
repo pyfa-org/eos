3050f7e6fd3ab8d685dc31a6b5ca331f1f3fbd0c
DarkPhoenix <phoenix@mail.ru>
2017-02-06 22:38:22 +0300

    Finish script which will help with const maintenance

--- scripts/unused_consts.py
+++ scripts/unused_consts.py
@@ -28,8 +28,8 @@ import os
 
 class EnumSeeker(ast.NodeVisitor):
 
-    def __init__(self, enums):
-        self.enums = enums
+    def __init__(self, enum_data):
+        self.enum_data = enum_data
 
     def visit_ClassDef(self, enum_class_node):
         # Check only enums inherited from IntEnum class
@@ -46,27 +46,129 @@ class EnumSeeker(ast.NodeVisitor):
             for enum_item_name_node in enum_assignment_node.targets:
                 if not isinstance(enum_item_name_node, ast.Name):
                     continue
-                item_names = self.enums.setdefault(enum_class_node.name, set())
+                # Get data about enum entries into container
+                item_names = self.enum_data.setdefault(enum_class_node.name, set())
                 item_names.add(enum_item_name_node.id)
 
 
-def get_enums(base_path):
+class ReferenceSeeker(ast.NodeVisitor):
+
+    def __init__(self, enum_data, used_enums):
+        self.enum_data = enum_data
+        self.used_enums = used_enums
+        # Format: {local enum name: original enum name}
+        self.alt_names = {}
+
+    def visit_Import(self, node):
+        self.__find_alt_name(node)
+
+    def visit_ImportFrom(self, node):
+        self.__find_alt_name(node)
+
+    def __find_alt_name(self, node):
+        for alias in node.names:
+            if alias.asname is None:
+                continue
+            if alias.name not in self.enum_data:
+                continue
+            self.alt_names[alias.asname] = alias.name
+
+    def visit_Attribute(self, node):
+        # Get enum name
+        rightmost = self.__get_rightmost_value(node.value)
+        if rightmost in self.alt_names:
+            enum_name = self.alt_names[rightmost]
+        elif rightmost in self.enum_data:
+            enum_name = rightmost
+        else:
+            return
+        # Get attr name
+        attr_name = node.attr
+        # If it's in enum, make sure it's marked as used
+        if attr_name in self.enum_data[enum_name]:
+            used_attrs = self.used_enums.setdefault(enum_name, set())
+            used_attrs.add(attr_name)
+
+    def __get_rightmost_value(self, node):
+        if isinstance(node, ast.Name):
+            return node.id
+        elif isinstance(node, ast.Attribute):
+            return self.__get_rightmost_value(node.value)
+        # We're not interested in all constructs besides obj1.obj2.obj3.attr,
+        # which are composed of Attribute and Name nodes, thus we're not
+        # interested in anything else
+        else:
+            return None
+
+
+def get_enum_data(root_dir):
     # Format: {enum name: {enum item names}}
-    enums = {}
+    enum_data = {}
     # Cycle through all .py files in consts directory
-    const_path = os.path.join(base_path, 'eos', 'const')
-    for dir_path, dirs, files in os.walk(const_path):
+    const_path = os.path.join(root_dir, 'eos', 'const')
+    for ast_root in _get_asts_in_dir(const_path):
+        EnumSeeker(enum_data).visit(ast_root)
+    return enum_data
+
+
+def get_used_enums(path, enum_data):
+    # Format: {enum name: {enum item names}}
+    used_enums = {}
+    for ast_root in _get_asts_in_dir(path):
+        ReferenceSeeker(enum_data, used_enums).visit(ast_root)
+    return used_enums
+
+
+def _get_asts_in_dir(path):
+    for dir_path, dirs, files in os.walk(path):
         for file in filter(lambda f: os.path.splitext(f)[1] == '.py', files):
             with open(os.path.join(dir_path, file)) as f:
                 data = f.read()
-            ast_root = ast.parse(data)
-            EnumSeeker(enums).visit(ast_root)
-    return enums
+            try:
+                ast_root = ast.parse(data)
+            except KeyboardInterrupt:
+                raise
+            except:
+                print('failed to parse source for {}'.format(os.path.join(dir_path, file)))
+            else:
+                yield ast_root
+
+
+def print_results(enum_data, header):
+    if len(enum_data) > 0:
+        print('{}:'.format(header))
+        for enum_name in sorted(enum_data):
+            print('  {}'.format(enum_name))
+            for attr_name in sorted(enum_data[enum_name]):
+                print('    {}'.format(attr_name))
 
 
 if __name__ == '__main__':
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    eos_dir = os.path.abspath(os.path.join(script_dir, '..'))
+    root_dir = os.path.abspath(os.path.join(script_dir, '..'))
 
-    enums = get_enums(eos_dir)
+    enum_data = get_enum_data(root_dir)
+    used_by_eos = get_used_enums(os.path.join(root_dir, 'eos'), enum_data)
+    used_by_tests = get_used_enums(os.path.join(root_dir, 'tests'), enum_data)
+
+    # Format: {enum name: {enum item names}}
+    unused = {}
+    only_tests = {}
+
+    for enum_name, enum_attrs in enum_data.items():
+        for enum_attr in enum_attrs:
+            # Used by eos
+            if enum_attr in used_by_eos.get(enum_name, ()):
+                continue
+            # Not used by eos, but used by tests
+            if enum_attr in used_by_tests.get(enum_name, ()):
+                only_test_attrs = only_tests.setdefault(enum_name, set())
+                only_test_attrs.add(enum_attr)
+                continue
+            # Not used at all
+            unused_attrs = unused.setdefault(enum_name, set())
+            unused_attrs.add(enum_attr)
+
+    print_results(unused, 'Enum entries which are not used at all')
+    print_results(only_tests, 'Enum entries which are used only in tests')
