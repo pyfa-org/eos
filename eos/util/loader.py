fe8bff92d6b5396118a13e3669a36eadd0b7f3c6
DarkPhoenix <phoenix@mail.ru>
2017-12-11 23:38:39 +0300

    Commit missing file

---
+++ eos/util/loader.py
@@ -0,0 +1,32 @@
+# ==============================================================================
+# Copyright (C) 2011 Diego Duclos
+# Copyright (C) 2011-2017 Anton Vorobyov
+#
+# This file is part of Eos.
+#
+# Eos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Eos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Eos. If not, see <http://www.gnu.org/licenses/>.
+# ==============================================================================
+
+
+import pkgutil
+
+
+def load_modules(path):
+    """Walk through specified path and load all modules within it.
+
+    Loads just top-level modules and packages, any package children should be
+    loaded by package itself.
+    """
+    for loader, name, is_pkg in pkgutil.iter_modules(path):
+        loader.find_module(name).load_module(name)
