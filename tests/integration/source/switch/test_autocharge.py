586148ca40a319901f8d7fc7d2eb191949170b15
DarkPhoenix <phoenix@mail.ru>
2017-12-14 14:16:32 +0300

    Add more autocharge source switch tests

--- tests/integration/source/switch/test_autocharge.py
+++ tests/integration/source/switch/test_autocharge.py
@@ -58,7 +58,7 @@ class TestSourceSwitchAutocharge(SourceTestCase):
 
     def test_autocharge_to_none_no_effect(self):
         # Attribute setup
-        src_attr_id = self.allocate_attr_id('src1')
+        src_attr_id = self.allocate_attr_id('src1', 'src2')
         tgt_attr_id = self.allocate_attr_id('src1', 'src2')
         self.mkattr(src='src1', attr_id=src_attr_id)
         self.mkattr(src='src2', attr_id=src_attr_id)
@@ -122,6 +122,227 @@ class TestSourceSwitchAutocharge(SourceTestCase):
         self.fit.source = 'src2'
         # Verification
         self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 10)
+        # Cleanup
+        self.assert_fit_buffers_empty(self.fit)
+        self.assertEqual(len(self.get_log()), 0)
+
+    def test_none_no_effect_to_autocharge(self):
+        # Attribute setup
+        src_attr_id = self.allocate_attr_id('src1', 'src2')
+        tgt_attr_id = self.allocate_attr_id('src1', 'src2')
+        self.mkattr(src='src1', attr_id=src_attr_id)
+        self.mkattr(src='src2', attr_id=src_attr_id)
+        self.mkattr(src='src1', attr_id=tgt_attr_id)
+        self.mkattr(src='src2', attr_id=tgt_attr_id)
+        # Autocharge setup
+        autocharge_modifier = self.mkmod_filter_item(src_attr_id, tgt_attr_id)
+        autocharge_effect_id = self.allocate_effect_id('src1', 'src2')
+        autocharge_effect_src1 = self.mkeffect(
+            src='src1',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_effect_src2 = self.mkeffect(
+            src='src2',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=autocharge_type_id,
+            attrs={src_attr_id: 50},
+            effects=[autocharge_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=autocharge_type_id,
+            attrs={src_attr_id: 50},
+            effects=[autocharge_effect_src2])
+        # Container setup
+        # Just create it to 2nd source, we're not going to use it
+        self.mkeffect_container_autocharge('src1')
+        container_effect_src2 = self.mkeffect_container_autocharge('src2')
+        container_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type_id})
+        self.mktype(
+            src='src2',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type_id},
+            effects=[container_effect_src2])
+        container_item = ModuleHigh(container_type_id, state=State.active)
+        # Influence target setup
+        influence_tgt_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 10})
+        self.mktype(
+            src='src2',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 10})
+        influence_tgt = Ship(influence_tgt_type_id)
+        # Fit setup
+        self.fit.ship = influence_tgt
+        self.fit.modules.high.append(container_item)
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 10)
+        # Action
+        self.fit.source = 'src2'
+        # Verification
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 15)
+        # Cleanup
+        self.assert_fit_buffers_empty(self.fit)
+        self.assertEqual(len(self.get_log()), 0)
+
+    def test_autocharge_to_autocharge_changed_attrs(self):
+        # Attribute setup
+        src_attr_id = self.allocate_attr_id('src1', 'src2')
+        tgt_attr_id = self.allocate_attr_id('src1', 'src2')
+        self.mkattr(src='src1', attr_id=src_attr_id)
+        self.mkattr(src='src2', attr_id=src_attr_id)
+        self.mkattr(src='src1', attr_id=tgt_attr_id)
+        self.mkattr(src='src2', attr_id=tgt_attr_id)
+        # Autocharge setup
+        autocharge_modifier = self.mkmod_filter_item(src_attr_id, tgt_attr_id)
+        autocharge_effect_id = self.allocate_effect_id('src1', 'src2')
+        autocharge_effect_src1 = self.mkeffect(
+            src='src1',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_effect_src2 = self.mkeffect(
+            src='src2',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=autocharge_type_id,
+            attrs={src_attr_id: 50},
+            effects=[autocharge_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=autocharge_type_id,
+            attrs={src_attr_id: 5},
+            effects=[autocharge_effect_src2])
+        # Container setup
+        container_effect_src1 = self.mkeffect_container_autocharge('src1')
+        container_effect_src2 = self.mkeffect_container_autocharge('src2')
+        container_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type_id},
+            effects=[container_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type_id},
+            effects=[container_effect_src2])
+        container_item = ModuleHigh(container_type_id, state=State.active)
+        # Influence target setup
+        influence_tgt_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 10})
+        self.mktype(
+            src='src2',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 200})
+        influence_tgt = Ship(influence_tgt_type_id)
+        # Fit setup
+        self.fit.ship = influence_tgt
+        self.fit.modules.high.append(container_item)
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 15)
+        # Action
+        self.fit.source = 'src2'
+        # Verification
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 210)
+        # Cleanup
+        self.assert_fit_buffers_empty(self.fit)
+        self.assertEqual(len(self.get_log()), 0)
+
+    def test_autocharge_to_autocharge_changed_type(self):
+        # Attribute setup
+        src_attr_id = self.allocate_attr_id('src1', 'src2')
+        tgt_attr_id = self.allocate_attr_id('src1', 'src2')
+        self.mkattr(src='src1', attr_id=src_attr_id)
+        self.mkattr(src='src2', attr_id=src_attr_id)
+        self.mkattr(src='src1', attr_id=tgt_attr_id)
+        self.mkattr(src='src2', attr_id=tgt_attr_id)
+        # Autocharge setup
+        autocharge_modifier = self.mkmod_filter_item(src_attr_id, tgt_attr_id)
+        autocharge_effect_id = self.allocate_effect_id('src1', 'src2')
+        autocharge_effect_src1 = self.mkeffect(
+            src='src1',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_effect_src2 = self.mkeffect(
+            src='src2',
+            effect_id=autocharge_effect_id,
+            category_id=EffectCategoryId.passive,
+            modifiers=[autocharge_modifier])
+        autocharge_type1_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=autocharge_type1_id,
+            attrs={src_attr_id: 50},
+            effects=[autocharge_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=autocharge_type1_id,
+            attrs={src_attr_id: 100},
+            effects=[autocharge_effect_src2])
+        autocharge_type2_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=autocharge_type2_id,
+            attrs={src_attr_id: 50},
+            effects=[autocharge_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=autocharge_type2_id,
+            attrs={src_attr_id: 100},
+            effects=[autocharge_effect_src2])
+        # Container setup
+        container_effect_src1 = self.mkeffect_container_autocharge('src1')
+        container_effect_src2 = self.mkeffect_container_autocharge('src2')
+        container_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type1_id},
+            effects=[container_effect_src1])
+        self.mktype(
+            src='src2',
+            type_id=container_type_id,
+            attrs={self.autocharge_attr_id: autocharge_type2_id},
+            effects=[container_effect_src2])
+        container_item = ModuleHigh(container_type_id, state=State.active)
+        # Influence target setup
+        influence_tgt_type_id = self.allocate_type_id('src1', 'src2')
+        self.mktype(
+            src='src1',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 10})
+        self.mktype(
+            src='src2',
+            type_id=influence_tgt_type_id,
+            attrs={tgt_attr_id: 10})
+        influence_tgt = Ship(influence_tgt_type_id)
+        # Fit setup
+        self.fit.ship = influence_tgt
+        self.fit.modules.high.append(container_item)
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 15)
+        # Action
+        self.fit.source = 'src2'
+        # Verification
+        self.assertAlmostEqual(influence_tgt.attrs[tgt_attr_id], 20)
         # Cleanup
         self.assert_fit_buffers_empty(self.fit)
         self.assertEqual(len(self.get_log()), 0)
