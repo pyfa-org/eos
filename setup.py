8e5f1e3ad4370926d07d2c55587fc55f16a92c10
Peter Heise <pheise.dev@gmail.com>
2018-05-04 13:54:39 +0200

    idk.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from eos import __version__
 try:  # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirement
+    from pip.req import parse_requirement  # noqa
 
 
 parsed_reqs = parse_requirements('requirements.txt', session=False)
