44c81d87f4d9d5432270bc2f0863a9f822414ffd
Peter Heise <pheise.dev@gmail.com>
2018-05-04 13:37:09 +0200

    Fixed setup.py for pip >= 10.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-
-
-from pip.req import parse_requirements
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from eos import __version__
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirement
+
 
 
 parsed_reqs = parse_requirements('requirements.txt', session=False)
